3d43b7cfb40135e58de4d42be60bbd0120b6f9e9
高原 <gaoyuan200408@credithc.com>
2022-08-13 10:09:32 +0800

    修改打包发布分支

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd docs/.vuepress/dist/
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/gy1001/gy1001.github.io.git gy-fly
+git push -f https://github.com/gy1001/gy1001.github.io.git main
 cd - 
