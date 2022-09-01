1aa4b1468aca2464c7a8c9b5f857ca9c92d6fd16
gaoyuan <gaoyuan_flying@163.com>
2022-09-01 18:55:17 +0800

    feat:this指针问题

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -66,7 +66,11 @@ export const sidebarZh: SidebarConfig = {
   '/zh/interview': [
     {
       text: 'JavaScript相关',
-      children: ['/zh/interview', '/zh/interview/javascript/new.md'],
+      children: [
+        '/zh/interview',
+        '/zh/interview/javascript/new.md',
+        '/zh/interview/javascript/this.md',
+      ],
     },
     {
       text: 'HTML相关',
