6b220c3df282af42bfef21acc386f354cdfc8fa7
MineMine <gaoyuan_flying@163.com>
2023-03-15 23:56:29 +0800

    添加目录

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -238,6 +238,7 @@ export const sidebarZh: SidebarConfig = {
             '/zh/other/class/vue3-learn/index-02.md',
             '/zh/other/class/vue3-learn/index-03.md',
             '/zh/other/class/vue3-learn/index-04.md',
+            '/zh/other/class/vue3-learn/index-05.md',
           ]
         }
       ],
