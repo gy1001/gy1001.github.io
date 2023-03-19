a397f298e55283ad8b1508e0abaca30ad1d73267
MineMine <gaoyuan_flying@163.com>
2023-03-19 17:40:45 +0800

    runtime 运行时-运行时核心设计原则

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -240,6 +240,7 @@ export const sidebarZh: SidebarConfig = {
             '/zh/other/class/vue3-learn/index-04.md',
             '/zh/other/class/vue3-learn/index-05.md',
             '/zh/other/class/vue3-learn/index-06.md',
+            '/zh/other/class/vue3-learn/index-07.md',
           ]
         }
       ],
