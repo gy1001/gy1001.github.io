02753e9bd9c6437cb36af2ea202b53fc05b81cf8
MineMine <gaoyuan_flying@163.com>
2023-05-11 16:18:20 +0800

    什么是前端性能？

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -276,6 +276,7 @@ export const sidebarZh: SidebarConfig = {
             '/zh/other/class/engineering/index-18.md',
             '/zh/other/class/engineering/index-19.md',
             '/zh/other/class/engineering/index-20.md',
+            '/zh/other/class/engineering/index-21.md',
           ],
         },
         {
