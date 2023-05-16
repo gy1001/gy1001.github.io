21ef7529a2ba7dfc04db0a01fa5d01a11c7ff1c8
MineMine <gaoyuan_flying@163.com>
2023-05-16 16:41:37 +0800

    泛型进阶

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -306,6 +306,7 @@ export const sidebarZh: SidebarConfig = {
             '/zh/other/class/typescript-advanced/index-03.md',
             '/zh/other/class/typescript-advanced/index-06.md',
             '/zh/other/class/typescript-advanced/index-07.md',
+            '/zh/other/class/typescript-advanced/index-08.md',
           ],
         },
       ],
