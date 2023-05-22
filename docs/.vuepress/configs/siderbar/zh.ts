d3084441e206e5c0e4d81b6deceb03edf71353e8
MineMine <gaoyuan_flying@163.com>
2023-05-22 18:05:06 +0800

    目录

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -335,6 +335,11 @@ export const sidebarZh: SidebarConfig = {
             '/zh/other/class/typescript-supplement/index-05.md',
           ],
         },
+        {
+          text: '破解JavaScript高级玩法，成为精通 JS 的原生专家',
+          collapsible: true,
+          children: ['/zh/other/class/crack-js/index-01.md'],
+        },
       ],
     },
   ],
