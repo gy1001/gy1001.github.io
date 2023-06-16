f519e0bc961a3fc0509f1b9f4e020a3627d9a50a
MineMine <gaoyuan_flying@163.com>
2023-06-16 11:14:59 +0800

    其他设计模式

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -389,6 +389,7 @@ export const sidebarZh: SidebarConfig = {
             '/zh/other/class/design-pattern/index-08.md',
             '/zh/other/class/design-pattern/index-09.md',
             '/zh/other/class/design-pattern/index-10.md',
+            '/zh/other/class/design-pattern/index-11.md',
           ],
         },
       ],
