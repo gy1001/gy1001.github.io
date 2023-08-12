24b232ab8819bef58e9e9f04fe08498c077b817e
MineMine <gaoyuan_flying@163.com>
2023-08-12 19:31:33 +0800

    feat:构建优化(揭开webpack性能优化的内幕)

--- docs/.vuepress/configs/siderbar/zh.ts
+++ docs/.vuepress/configs/siderbar/zh.ts
@@ -461,6 +461,7 @@ export const sidebarZh: SidebarConfig = {
             '/zh/other/class/performance-optimization-standard/index-03.md',
             '/zh/other/class/performance-optimization-standard/index-04.md',
             '/zh/other/class/performance-optimization-standard/index-05.md',
+            '/zh/other/class/performance-optimization-standard/index-06.md',
           ],
         },
         {
